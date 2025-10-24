7365514f6783b115194ce7ccedbd7f9fb0606a72
khalisha kaylanasywa <kkaylanasywa>
2025-10-24 10:10:18 +0700

    feat: [KK] add button logout to profile screen

--- lib/features/profile/profile_screen.dart
+++ lib/features/profile/profile_screen.dart
@@ -1,38 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:shantika_cubit/features/profile/informasi_pribadi_screen.dart';
-import 'package:shantika_cubit/utility/extensions/date_time_extensions.dart';
 import 'package:shantika_cubit/utility/extensions/show_toast.dart';
-import 'package:shantika_cubit/utility/extensions/widget_extensions.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:with_space_between/with_space_between.dart';
 
 import '../../config/constant.dart';
 import '../../config/service_locator.dart';
 import '../../config/user_preference.dart';
-import '../../model/user_model.dart';
 import '../../ui/color.dart';
 import '../../ui/dimension.dart';
 import '../../ui/shared_widget/circle_image_view.dart';
 import '../../ui/shared_widget/custom_button.dart';
 import '../../ui/shared_widget/custom_card.dart';
-import '../../ui/shared_widget/error_view.dart';
-import '../../ui/shared_widget/reuse_menu_profile.dart';
 import '../../ui/typography.dart';
 import '../../utility/loading_overlay.dart';
-// import '../article/article_screen.dart';
 import '../authentication/login/login_screen.dart';
-// import '../notification/notification_screen.dart';
-// import '../promo/promo_screen.dart';
-import 'cubit/logout_cubit.dart';
-import 'cubit/profile_cubit.dart';
-import 'my_profile_screen.dart';
 
-// ignore: must_be_immutable
 class ProfileScreen extends StatelessWidget {
   ProfileScreen({super.key});
+
+  final loadingOverlay = LoadingOverlay();
 
   @override
   Widget build(BuildContext context) {
@@ -43,22 +31,18 @@ class ProfileScreen extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              SizedBox(height: space800),
+              SizedBox(height: spacing4),
               CircleAvatar(
                 radius: 50,
-                backgroundImage: AssetImage('assets/images/img_profil_default.jpg'),
+                backgroundImage:
+                AssetImage('assets/images/img_profil_default.jpg'),
                 backgroundColor: black500,
               ),
               SizedBox(height: spacing5),
-              Text(
-                "Anastasya Carolina",
-                style: mdBold,
-              ),
+              Text("Anastasya Carolina", style: mdBold),
               SizedBox(height: space150),
-              Text(
-                "087374543899",
-                style: smMedium.copyWith(color: black400),
-              ),
+              Text("087374543899",
+                  style: smMedium.copyWith(color: black400)),
               SizedBox(height: spacing7),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: padding20),
@@ -77,92 +61,48 @@ class ProfileScreen extends StatelessWidget {
                       },
                     ),
                     SizedBox(height: spacing4),
-
                     _menuItem(
                       svgIcon: 'assets/icons/notif.svg',
                       text: "Notifikasi",
-                      onTap: () {
-                        // Get.to(() => NotifikasiPage());
-                      },
+                      onTap: () {},
                     ),
                     SizedBox(height: spacing4),
-
                     _menuItem(
                       svgIcon: 'assets/icons/info.svg',
                       text: "Tentang Kami",
-                      onTap: () {
-                        // Get.to(() => TentangKamiPage());
-                      },
+                      onTap: () {},
                     ),
                     SizedBox(height: spacing4),
-
                     _menuItem(
                       svgIcon: 'assets/icons/check.svg',
                       text: "Kebijakan Privasi",
-                      onTap: () {
-                        // Get.to(() => KebijakanPrivasiPage());
-                      },
+                      onTap: () {},
                     ),
                     SizedBox(height: spacing4),
-
                     _menuItem(
                       svgIcon: 'assets/icons/note.svg',
                       text: "Syarat dan Ketentuan",
-                      onTap: () {
-                        // Get.to(() => SnkPage());
-                      },
+                      onTap: () {},
                     ),
                     SizedBox(height: spacing4),
-
                     _menuItem(
                       svgIcon: 'assets/icons/faq.svg',
                       text: "FAQ",
-                      onTap: () {
-                        // Get.to(() => FAQPage());
-                      },
+                      onTap: () {},
                     ),
                     SizedBox(height: spacing4),
-
                     _menuItem(
                       svgIcon: 'assets/icons/stars.svg',
                       text: "Beri Nilai App Kami",
-                      trailing: Text(
-                        "Versi 1.20.5",
-                        style: xxsMedium,
-                      ),
-                      onTap: () {
-                        // Launch app store or play store
-                      },
+                      trailing: Text("Versi 1.20.5", style: xxsMedium),
+                      onTap: () {},
                     ),
                     SizedBox(height: spacing5),
-
                     CustomButton(
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => LoginScreen(),
-                          ),
-                        );
-                        // UserPreference userPreference = serviceLocator.get<UserPreference>();
-                            // userPreference.clearData();
-                            // _logoutCubit.logout();
-                      },
+                      onPressed: () => _showLogoutDialog(context),
                       child: Text('Keluar Akun'),
                     ),
                     SizedBox(height: spacing5),
-
-                    // Logout Button
-                    // ReuseButton(
-                    //   text: "Keluar Akun",
-                    //   onPressed: () {
-                    //     controller.logout();
-                    //     // Logout logic here
-                    //   },
-                    //   backgroundColor: AppStyles.logout,
-                    //   radius: AppStyles.radiusL,
-                    // ),
-                    SizedBox(height: space050),
                   ],
                 ),
               ),
@@ -178,21 +118,18 @@ class ProfileScreen extends StatelessWidget {
       preferredSize: Size.fromHeight(kToolbarHeight + 4),
       child: Container(
         decoration: BoxDecoration(
-          color:black00,
+          color: black00,
           boxShadow: [
             BoxShadow(
-              color: black950.withOpacity(0.08),
-              blurRadius: 8,
-              offset: Offset(0, 3),
+              color: black950.withOpacity(0.09),
+              blurRadius: 4,
+              offset: Offset(0, 2),
             ),
           ],
         ),
         child: AppBar(
           backgroundColor: Colors.transparent,
-          title: Text(
-            "Profil",
-            style: xlBold,
-          ),
+          title: Text("Profil", style: xlBold),
           centerTitle: true,
         ),
       ),
@@ -207,43 +144,123 @@ class ProfileScreen extends StatelessWidget {
   }) {
     return CustomCard(
       onTap: onTap,
-      color: navy200.withOpacity(0.6),
+      color: navy200,
       borderRadius: BorderRadius.circular(borderRadius300),
       padding: EdgeInsets.symmetric(
         horizontal: paddingM,
         vertical: paddingM,
       ),
-      child: ClipRRect(
-        child: Row(
-          children: [
-            SvgPicture.asset(
-              svgIcon,
-              height: 24,
-              colorFilter: const ColorFilter.mode(
-                black950,
-                BlendMode.srcIn,
-              ),
+      child: Row(
+        children: [
+          SvgPicture.asset(
+            svgIcon,
+            height: iconL,
+            colorFilter: ColorFilter.mode(
+              black950,
+              BlendMode.srcIn,
             ),
-            SizedBox(width: spacing4),
-            Expanded(
-              child: Text(
-                text,
-                style: smSemiBold,
-              ),
-            ),
-            if (trailing != null) ...[
-              trailing,
-              SizedBox(width: space050),
-            ],
-            Icon(
-              Icons.chevron_right,
-              color: black800,
-              size: 24,
-            ),
+          ),
+          SizedBox(width: spacing4),
+          Expanded(
+            child: Text(text, style: smSemiBold),
+          ),
+          if (trailing != null) ...[
+            trailing,
+            SizedBox(width: space050),
           ],
-        ),
+          Icon(Icons.arrow_forward_ios_rounded,
+              color: black800, size: iconL),
+        ],
       ),
     );
   }
 
+  void _showLogoutDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          shape:
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          backgroundColor: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.all(padding20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 48,
+                  height: 48,
+                  decoration: BoxDecoration(
+                    color: Colors.red.withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(28),
+                  ),
+                  child: Icon(Icons.logout, color: Colors.red, size: 28),
+                ),
+                SizedBox(height: 16),
+                Text(
+                  "Keluar Akun",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  "Yakin Anda akan keluar dari akun Anastasya Carolina?",
+                  style: TextStyle(color: Colors.grey[700], fontSize: 14),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  children: [
+                    Expanded(
+                      child: OutlinedButton(
+                        onPressed: () => Navigator.pop(context),
+                        style: OutlinedButton.styleFrom(
+                          side: BorderSide(color: Colors.black26),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        child: Text("Batal",
+                            style: TextStyle(color: Colors.black)),
+                      ),
+                    ),
+                    SizedBox(width: 12),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          Navigator.pop(context);
+
+                          final userPreference =
+                          serviceLocator.get<UserPreference>();
+                          await userPreference.clearData();
+
+                          Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(
+                                builder: (context) => LoginScreen()),
+                                (route) => false,
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        child: Text("Keluar",
+                            style: TextStyle(color: Colors.white)),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
